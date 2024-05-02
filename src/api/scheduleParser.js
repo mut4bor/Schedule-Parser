08ee3ce8f2f7169b4eff32ea832bbd8bd7c26782
mut4bor <matvei.molchanov2004@gmail.com>
2024-05-02 04:19:25 +0300

    i suck at array manipulating

--- src/api/scheduleParser.js
+++ src/api/scheduleParser.js
@@ -2,40 +2,40 @@ import excelToJson from 'convert-excel-to-json'
 import path from 'path'
 
 const __dirname = path.resolve()
-const filePath = path.join(__dirname, 'fileBase', 'schedule_8.xlsx')
+const filePath = path.join(__dirname, 'fileBase', 'schedule_6.xlsx')
 
-// https://www.npmjs.com/package/convert-excel-to-json
-
-export const result = excelToJson({
+const unParsedJson = excelToJson({
   sourceFile: filePath,
   header: {
     rows: 5,
   },
   range: 'A1:Z35',
 })
-export const content = Object.values(result).map((item) => {
+
+const parsedJson = {}
+Object.values(unParsedJson).forEach((item, index) => {
   const group = item.map((item, index) => {
-    const date = item.A
-    const day = item.B
-    const time = item.C
-    const name = item.F || ''
-    const classDate = date || day ? `${date} - ${day}` : undefined
-    const classTimeName = time || name ? `${time} - ${name}` : undefined
+    const { A: date, B: day, C: time, F: name } = item
+    const classDate = `${date ? date : ''} - ${day ? day : ''}`
+    const classTimeName = `${time ? time : ''} - ${name ? name : ''}`
     return {
       date: classDate,
       class: classTimeName,
     }
   })
-  const combined = []
+
+  const combined = {}
   for (let i = 0; i < group.length; i += 5) {
-    combined.push({
-      date: group[i]?.date,
+    combined[group[i]?.date] = {
       1: group[i]?.class,
       2: group[i + 1]?.class,
       3: group[i + 2]?.class,
       4: group[i + 3]?.class,
       5: group[i + 4]?.class,
-    })
+    }
   }
-  return combined
+  const key = Object.keys(unParsedJson)[index]
+  parsedJson[key] = combined
 })
+
+export { parsedJson, unParsedJson }
