77813ab3eb4008813a77acd7929c99125eea04cc
mut4bor <matvei.molchanov2004@gmail.com>
2024-05-10 00:04:00 +0300

    finally works properly with first 8 files

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 import express from 'express'
 import path from 'path'
-import { parsedJson, unParsedJson } from './src/api/scheduleParser.js'
+import { allData } from './src/api/scheduleParser.js'
 const app = express()
 const __dirname = path.resolve()
 const PORT = process.env.PORT || 3000
 
 app.get('/api/schedule/parsed', (req, res) => {
-  res.status(200).json(parsedJson)
-})
-app.get('/api/schedule/unparsed', (req, res) => {
-  res.status(200).json(unParsedJson)
+  res.status(200).json(allData)
 })
 
 app.use(express.static(path.resolve(__dirname, 'static')))
--- src/api/scheduleParser.js
+++ src/api/scheduleParser.js
@@ -2,82 +2,111 @@ import excelToJson from 'convert-excel-to-json'
 import path from 'path'
 
 const __dirname = path.resolve()
-const filePath = path.join(__dirname, 'fileBase', 'schedule_2.xlsx')
+const fileNames = [
+  'schedule_1.xlsx',
+  'schedule_2.xlsx',
+  'schedule_3.xlsx',
+  'schedule_4.xlsx',
+  'schedule_5.xlsx',
+  'schedule_6.xlsx',
+  'schedule_7.xlsx',
+  'schedule_8.xlsx',
+]
 
-const unParsedJson = excelToJson({
-  sourceFile: filePath,
-  header: {
-    rows: 3,
-  },
-  range: 'A1:Z35',
-})
-const groupedData = {}
-const parsedJson = {}
-const weeks = {}
-
-const flatten = (arr) => arr.reduce((acc, item) => acc.concat(item), [])
-
-const groupByDay = (data, groupNumber) => {
-  let currentDay = null
-  const groupLetter = getGroupKeys()[groupNumber]
-  data.forEach((item) => {
-    const group = item[groupLetter] || ''
-    if (item.A) {
-      currentDay = `${item.A}(${item.B})`
-      groupedData[currentDay] = { [item.C]: group }
-    } else if (currentDay) {
-      groupedData[currentDay] = { ...groupedData[currentDay], [item.C]: group }
-    }
+const processDataForFile = (fileName) => {
+  const filePath = path.join(__dirname, 'fileBase', fileName)
+  const unParsedJson = excelToJson({
+    sourceFile: filePath,
+    header: {
+      rows: 3,
+    },
+    range: 'A1:Z35',
   })
-}
 
-const getDateKeys = (data) => {
-  const dates = []
-  Object.keys(data).forEach((key) => {
-    const formattedKey = key.replaceAll('.', '').replaceAll(' ', '')
-    const keyWithDots = formattedKey.slice(0, 2) + '.' + formattedKey.slice(2, -2) + '.' + formattedKey.slice(-2)
-    dates.push(keyWithDots)
-  })
-  return dates
-}
+  const flatten = (arr) => arr.reduce((acc, item) => acc.concat(item), [])
 
-const groupByWeek = (groupedData) => {
-  let currentWeek = {}
-  let weekIndex = 0
-  const dateKeys = getDateKeys(unParsedJson)
-  Object.entries(groupedData).forEach(([date, times]) => {
-    if (date.includes('Пн.')) {
-      currentWeek = {}
-      const key = dateKeys[weekIndex++]
-      weeks[key] = currentWeek
-    }
-    currentWeek[date] = times
-  })
-}
-
-const getGroupKeys = () => {
-  const swapped = {}
-  const obj = flatten(Object.values(unParsedJson))[0]
-  for (const key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      const value = obj[key].match(/\d+/)
-      if (value !== null) {
-        swapped[value] = key
+  const getGroupKeys = () => {
+    const swapped = {}
+    const obj = flatten(Object.values(unParsedJson))[0]
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        const value = obj[key].match(/\d+/)
+        if (value !== null) {
+          swapped[value] = key
+        }
       }
     }
+    return swapped
   }
-  return swapped
+
+  const getDateKeys = () => {
+    const dates = {}
+    Object.keys(unParsedJson).forEach((key) => {
+      const formattedKey = key.replaceAll('.', '').replaceAll(' ', '')
+      const keyWithDots = formattedKey.slice(0, 2) + '.' + formattedKey.slice(2, -2) + '.' + formattedKey.slice(-2)
+      dates[keyWithDots] = {}
+    })
+    return dates
+  }
+
+  const extractGroupedData = (data, groupLetter) => {
+    const groupedData = {}
+    let currentDay = null
+    data.forEach((item) => {
+      const group = item[groupLetter] || ''
+      if (item.A) {
+        currentDay = `${item.A.substring(0, 5)} (${item.B})`
+        groupedData[currentDay] = { [item.C]: group }
+      } else if (currentDay) {
+        groupedData[currentDay] = { ...groupedData[currentDay], [item.C]: group }
+      }
+    })
+    return groupedData
+  }
+
+  const getGroupWeekDays = (groupNumber) => {
+    const data = flatten(Object.values(unParsedJson))
+    const groupLetter = getGroupKeys()[groupNumber]
+    const groupedData = extractGroupedData(data, groupLetter)
+    const weekDates = getDateKeys()
+    const weekDatesKeys = Object.keys(weekDates)
+    let weekIndex = 0
+    let currentWeek = {}
+
+    Object.entries(groupedData).forEach(([date, schedule]) => {
+      if (date.includes('Пн.')) {
+        const weekKey = weekDatesKeys[weekIndex++]
+        currentWeek = {}
+        weekDates[weekKey] = currentWeek
+      }
+      currentWeek[date] = schedule
+    })
+    return weekDates
+  }
+
+  const processData = () => {
+    const data = {}
+    const groupKeys = Object.keys(getGroupKeys())
+
+    groupKeys.forEach((groupKey) => {
+      data[groupKey] = getGroupWeekDays(groupKey)
+    })
+    return data
+  }
+
+  return processData()
 }
 
-const processData = () => {
-  const data = flatten(Object.values(unParsedJson))
-  groupByDay(data, 203)
-  groupByWeek(groupedData)
-  Object.keys(getGroupKeys(unParsedJson)).forEach((key) => {
-    parsedJson[key] = weeks
+const processDataForAllFiles = () => {
+  let data = {}
+  fileNames.forEach((fileName) => {
+    Object.entries(processDataForFile(fileName)).forEach(([group, schedule]) => {
+      data[group] = schedule
+    })
   })
+  return data
 }
 
-processData()
+const allData = processDataForAllFiles()
 
-export { parsedJson, unParsedJson }
+export { allData }
