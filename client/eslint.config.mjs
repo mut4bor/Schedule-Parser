7f61bbba8f47d7a1fa47a0ab9675932b61d1c0ee
mut4bor <matvei.molchanov2004@gmail.com>
2024-05-18 04:04:02 +0300

    mongodb successfully integrated on basic level

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -1,34 +1,34 @@
-import {FlatCompat} from "@eslint/eslintrc";
-import path from "path";
-import {fileURLToPath} from "url";
+import { FlatCompat } from '@eslint/eslintrc';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const __filename= fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname
+  baseDirectory: __dirname,
 });
 
 export default [
   ...compat.env({
     browser: true,
     es2020: true,
-    node: true
+    node: true,
   }),
 
-  ...compat.plugins( "react"),
+  ...compat.plugins('react'),
 
   ...compat.config({
-    plugins: ["@typescript-eslint", "react"],
+    plugins: ['@typescript-eslint', 'react'],
     env: {
       browser: true,
       es2020: true,
-      node: true
+      node: true,
     },
     rules: {
-      semi: "error",
-      "react/react-in-jsx-scope": "off",
-      "react/jsx-uses-react": "off"
-    }
-  })
+      semi: 'error',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+    },
+  }),
 ];
